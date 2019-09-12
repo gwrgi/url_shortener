f3961832142ce011971e89d45b8f0b81c4a6d1ed
Dan Stevens <dan.k.stevens@gmail.com>
2019-09-11 22:05:27 -0700

    Adding integration tests

--- testdata/curl_tests.sh
+++ testdata/curl_tests.sh
@@ -1,10 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-curl -X GET http://localhost:8080/v1/ping
+RESULT=`curl --disable -s -X GET http://localhost:8080/v1/ping`
+if [ "$RESULT" != "pong" ]; then
+    echo "Actual  : '$RESULT'"
+    echo "Expected: 'pong'"
+    exit 1
+fi
+# $RESULT should be pong
 
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://www.urlencoder.org/"}'
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://www.urlencoder.org/"}'
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://example.com"}'
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://www.duckduckgo.com/"}'
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://duckduckgo.com/?q=oh+the+humanity&t=ffsb&ia=web"}'
-curl -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"https://www.google.com/search?hl=en&q=https%3A%2F%2Fduckduckgo.com%2F%3Fq%3Dlorem%2Bipsum%26t%3Dffsb%26ia%3Danswer"}'
+urls=(
+    "http://example.com" # Simple URL
+    "https://example.com" # For Security
+    "https://www.duckduckgo.com/" # Ends with a /
+    "https://duckduckgo.com/?q=oh+the+humanity&t=ffsb&ia=web" # Includes parameters
+    "https://www.google.com/search?hl=en&q=https%3A%2F%2Fduckduckgo.com%2F%3Fq%3Dlorem%2Bipsum%26t%3Dffsb%26ia%3Danswer" # Includes url encoding
+    # TODO: Add the following tests
+    #   Really super long
+)
+
+for url in "${urls[@]}"
+do
+    SHORT_URL=`curl --disable -s -X POST 'http://localhost:8080/v1/create' -d '{"longUrl":"'$url'"}'`
+    ACTUAL=`curl --disable -s -X GET $SHORT_URL`
+
+    EXPECTED="<a href=\""$url"\">Temporary Redirect</a>."
+
+    if [ "$ACTUAL" != "$EXPECTED" ]; then
+        echo "Actual  : '$ACTUAL'"
+        echo "Expected: '$EXPECTED'"
+        exit 1
+    fi
+done
+
