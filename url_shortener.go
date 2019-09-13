59f9c570f1853371b120fcbd46720ad955ca7a58
Dan Stevens <dan.k.stevens@gmail.com>
2019-09-12 21:45:06 -0700

    Replaced map[string]string with sqlite

--- url_shortener.go
+++ url_shortener.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"errors"
 	"fmt"
-	"github.com/asaskevich/govalidator"
 	"github.com/julienschmidt/httprouter"
+	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
+	"time"
 )
 
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const shortenedUrlLength = 16
+const letterBytes = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const shortenedUrlLength = 7
+
+var database *sql.DB
+var router *httprouter.Router
 
 func generateShortUrlId() string {
 	b := make([]byte, shortenedUrlLength)
@@ -23,26 +29,102 @@ func generateShortUrlId() string {
 }
 
 type ShortenedUrl struct {
-	LongUrl string `json: "longUrl"`
+	ShortId       string `json: "shortId"`
+	LongUrl       string `json: "longUrl"`
+	Visits24Hours int    `json: "visits24Hours"`
+	Visits7Days   int    `json: "visits7Days"`
+	VisitsAllTime  int    `json: "visitsAllTime"`
 }
 
-var urlMap map[string]string
-var router *httprouter.Router
-
-func doesShortenedUrlIdExist(shortenedUrlId string) bool {
-	if _, ok := urlMap[shortenedUrlId]; ok {
-		return true
+func doesShortenedUrlIdExist(shortenedUrlId string) (bool, error) {
+	var count int
+	err := database.QueryRow("SELECT COUNT(1) FROM urlmap WHERE shorturlid = ?", shortenedUrlId).Scan(&count)
+	if err != nil {
+		return false, errors.New("Database error occurred: 0x2")
 	}
 
-	return false
+	if count > 0 {
+		return true, nil
+	}
+
+	return false, nil
+}
+
+func saveHistory(shortUrlId string) error {
+	visitedTime := time.Now().Unix()
+
+	statement, err := database.Prepare("INSERT INTO history (shorturlid, visited) VALUES (?, ?)")
+	if err == nil {
+		_, err = statement.Exec(shortUrlId, visitedTime)
+	}
+
+	return err
+}
+
+/*
+  Retrieves the number of times a given short url has been vistied since now minus duration seconds.
+  If duration is negative, count of all visits is returned
+*/
+func getHistoryCount(shortUrlId string, duration int64) (int, error) {
+	var since int64 = 0
+	if duration > 0 {
+		since = time.Now().Unix() - duration
+	}
+	
+	var count int
+	err := database.QueryRow("SELECT COUNT(1) FROM history WHERE shorturlid = ? AND visited > ?", shortUrlId, since).Scan(&count)
+	if err != nil {
+		return -1, errors.New("Database error occurred: 0x3")
+	}
+
+	return count, nil
 }
 
 func getLongUrl(shortUrlId string) (string, error) {
-	if longUrl, ok := urlMap[shortUrlId]; ok {
-		return longUrl, nil
+	rows, err := database.Query("SELECT longurl FROM urlmap WHERE shorturlid = ?", shortUrlId)
+	defer rows.Close()
+
+	if err != nil {
+		return "", errors.New("Database error occurred: 0x1")
 	}
 
-	return "", errors.New("No such URL exists")
+	var longUrl string
+	for rows.Next() {
+		rows.Scan(&longUrl)
+		break // Should only be one...
+	}
+
+	return longUrl, nil
+}
+
+func insertIntoDb(shortUrlId, longUrl string) error {
+	statement, err := database.Prepare("INSERT INTO urlmap (shorturlid, longurl) VALUES (?, ?)")
+	if err == nil {
+		_, err = statement.Exec(shortUrlId, longUrl)
+	}
+
+	return err
+}
+
+func saveUrl(longUrl string) (string, error) {
+	var shortId string
+	for {
+		shortId = generateShortUrlId()
+		exists, err := doesShortenedUrlIdExist(shortId)
+		if err != nil {
+			return "", err
+		}
+		if !exists {
+			break
+		}
+	}
+
+	err := insertIntoDb(shortId, longUrl)
+	if err != nil {
+		return "", err
+	}
+
+	return shortId, nil
 }
 
 func longUrlHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
@@ -55,52 +137,149 @@ func longUrlHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 		return
 	}
 
-	if !govalidator.IsURL(urlReq.LongUrl) {
+	_, err = url.ParseRequestURI(urlReq.LongUrl)
+	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "Invalid Long Url")
 		return
 	}
 
-	var shortenedUrlId string
-	for {
-		shortenedUrlId = generateShortUrlId()
-		if !doesShortenedUrlIdExist(shortenedUrlId) {
-			break
-		}
-	}
+	shortId, err := saveUrl(urlReq.LongUrl)
 
-	shortenedUrl := "http://localhost:8080/" + shortenedUrlId
+	router.GET("/"+shortId, shortUrlHandler)
 
-	// TODO: Store this in a database, not in memory, or else it won't survive restarts
-	urlMap[shortenedUrlId] = urlReq.LongUrl
-
-	router.GET("/"+shortenedUrlId, shortUrlHandler)
-
-	fmt.Fprintf(w, shortenedUrl)
+	shortUrl := "http://localhost:8080/" + shortId
+	fmt.Fprintf(w, shortUrl)
 }
 
 func shortUrlHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	urlId := r.URL.Path[len("/"):]
 	longUrl, err := getLongUrl(urlId)
 
+	err = saveHistory(urlId)
 	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
+		// This is not great, and should be looked into if it ever occurs, but should NOT
+		// stop the user from getting their long url
+		log.Printf("Error occurred while saving history. urlId: '%s', err: %s", urlId, err)
+	}
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	if longUrl == "" {
+		// This represents a buggy state.  The router does not have wildcards, so the only way to get to this function is by having the urlId
+		// actually have been seen before.  If the longUrl it maps to is empty, then either an empty longUrl was erroneously accepted, or
+		// somehow it didn't get saved to the database.  As far as the user is concerned, however, the url they are looking for was not found
+		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	http.Redirect(w, r, longUrl, http.StatusTemporaryRedirect)
 }
 
+func infoHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	urlId := ps.ByName("shortId")
+	longUrl, err := getLongUrl(urlId)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	visits24Hours, err := getHistoryCount(urlId, 60 * 60 * 24)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	visits7Days, err := getHistoryCount(urlId, 60 * 60 * 24 * 7)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	visitsAllTime, err := getHistoryCount(urlId, -1)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
+	data := ShortenedUrl{
+		LongUrl: longUrl,
+		ShortId: urlId,
+		Visits24Hours: visits24Hours,
+		Visits7Days: visits7Days,
+		VisitsAllTime: visitsAllTime,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
+
 func pingHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "pong")
 }
 
-func main() {
-	urlMap = make(map[string]string)
+func initializeDatabase() error {
+	db, err := sql.Open("sqlite3", "./database.db")
+	database = db
 
+	if err == nil {
+		createUrlMapStatment, err := database.Prepare("CREATE TABLE IF NOT EXISTS urlmap (id INTEGER PRIMARY KEY, shorturlid TEXT, longurl TEXT)")
+		createHistoryStatment, err := database.Prepare("CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY, shorturlid TEXT, visited INTEGER)")
+
+		if err == nil {
+			_, err = createUrlMapStatment.Exec()
+			if err == nil {
+				_, err = createHistoryStatment.Exec()
+			}
+		}
+
+	}
+
+	return err
+}
+
+func addExistingRoutesFromDatabase() error {
+	rows, err := database.Query("SELECT shorturlid FROM urlmap")
+	if err == nil {
+		defer rows.Close()
+		var shortUrlId string
+		for rows.Next() {
+			rows.Scan(&shortUrlId)
+			router.GET("/"+shortUrlId, shortUrlHandler)
+		}
+	}
+
+	return err
+}
+
+func main() {
 	router = httprouter.New()
 	router.GET("/v1/ping", pingHandler)
 	router.POST("/v1/create", longUrlHandler)
+	router.GET("/v1/get/*shortId", infoHandler)
+
+	err := initializeDatabase()
+	if err != nil {
+		log.Fatal("An error occurred in database initialization: %s", err.Error())
+		return
+	}
+
+	err = addExistingRoutesFromDatabase()
+	if err != nil {
+		log.Fatal("An error occurred while setting up existing routes: %s", err.Error())
+		return
+	}
+
+	log.Printf("Ready...")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
